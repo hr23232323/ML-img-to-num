7e866f0324058237a641acb5fbf8a322ae746eec
BENSLAPTOP\benjamin <bdshaffer31@gmail.com>
2018-02-27 17:50:31 -0500

    Added decision tree, still working through some issues

--- decision_tree.py
+++ decision_tree.py
@@ -1,9 +1,6 @@
 import time
-
-#from bokeh.plotting import figure, output_file, show as plt
-import numpy as np
-from numpy import multiarray
 import matplotlib.pyplot as plt
+import numpy as np
 
 from sklearn.datasets import fetch_mldata
 from sklearn.linear_model import LogisticRegression
@@ -11,10 +8,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.utils import check_random_state
 
+
+
 print(__doc__)
 
 # Author: Arthur Mensch <arthur.mensch@m4x.org>
 # License: BSD 3 clause
+
+
 
 # Turn down for faster convergence
 t0 = time.time()
@@ -29,17 +30,19 @@ X = X[permutation]
 y = y[permutation]
 X = X.reshape((X.shape[0], -1))
 
+print(X)
+
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, train_size=train_samples, test_size=10000)
+    X, y, train_size=train_samples, test_size=6500)  # test size = 10000
 
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# Turn up tolerance for faster convergence
+# Turn up tolerance for faster convergence USED TO BE 0.1
 clf = LogisticRegression(C=50. / train_samples,
                          multi_class='multinomial',
-                         penalty='l1', solver='saga', tol=0.1)
+                         penalty='l1', solver='saga', tol=0.5)
 clf.fit(X_train, y_train)
 sparsity = np.mean(clf.coef_ == 0) * 100
 score = clf.score(X_test, y_test)
@@ -56,8 +59,8 @@ for i in range(10):
                    cmap=plt.cm.RdBu, vmin=-scale, vmax=scale)
     l1_plot.set_xticks(())
     l1_plot.set_yticks(())
-    l1_plot.set_xlabel('Class %i' % i)
-plt.suptitle('Classification vector for...')
+    l1_plot.set_xlabel(i)#'Class %i' %
+plt.suptitle('Classification Vectors:')
 
 run_time = time.time() - t0
 print('Example run in %.3f s' % run_time)
