bd776d8ae859db62f051fadab4bcf66089e28fb8
Harsh Rana <hr23232323@gmail.com>
2018-02-27 17:49:37 -0500

    ANN Working, Preprocessing working

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
 import numpy as np
 from keras.utils import to_categorical
+import matplotlib.pyplot as plt
 
 
 
@@ -7,19 +8,20 @@ from keras.utils import to_categorical
 def load_data():
 
 	# Load data
-	labels = np.Load('labels.npy')
-	inputs = np.Load('images.npy')
+	labels = np.load('labels.npy')
+	inputs = np.load('images.npy')
+
 
 	# Convert input matrix to vectors
-	flattened_input = np.reshape(inputs, (6500,784))
+	#flattened_input = np.reshape(inputs, (6500,784))
 
 	# Convert output labels to categorical vectors
 	encoded_labels = to_categorical(labels, num_classes=10)
 
 	# Bad input output
 	# TODO: Stratified sampling
-	itrain, ivalidate, itest = flattened_input[(0:(0.6*6500)), :], flattened_input[((0.6*6500):(0.75*6500)), :], flattened_input[((0.75*6500):6500), :]
-	ltrain, lvalidate, ltest = encoded_labels[(0:(0.6*6500)), :], encoded_labels[((0.6*6500):(0.75*6500)), :], encoded_labels[((0.75*6500):6500), :]
+	itrain, ivalidate, itest = inputs[:int(0.6*6500), :], inputs[int(0.6*6500):int(0.75*6500), :], inputs[int(0.75*6500):, :]
+	ltrain, lvalidate, ltest = encoded_labels[:int(0.6*6500), :], encoded_labels[int(0.6*6500):int(0.75*6500), :], encoded_labels[int(0.75*6500):6500, :]
 
 	# Return input (train, validate, test), output (train, validate, test)
 	return itrain, ivalidate, itest, ltrain, lvalidate, ltest
